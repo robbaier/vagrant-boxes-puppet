59d47426ee39d382db1439b990957740415dd662
Rob <rob@robbaier.com>
2016-09-16 07:54:46 -0500

    Cleaning up names

--- packer/build-drupal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-: ${BUILD_VERSION:="v$(date +'%Y%m%d')"}
-export BUILD_VERSION
-
-rm -i builds/centos-6.8-drupal.box
-packer build centos-6.8-drupal.json
-shasum -a 256 builds/centos-6.8-drupal.box
--- packer/build-wordpress.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-: ${BUILD_VERSION:="v$(date +'%Y%m%d')"}
-export BUILD_VERSION
-
-rm -i builds/centos-6.8-wordpress.box
-packer build centos-6.8-wordpress.json
-shasum -a 256 builds/centos-6.8-wordpress.box
