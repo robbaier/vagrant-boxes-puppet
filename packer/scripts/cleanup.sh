92e5499c0f484d768d77b88a69fe10866a5ebca9
Rob <rob@robbaier.com>
2016-09-20 15:45:53 -0500

    Remove documentation and locales during cleanup process

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -7,6 +7,13 @@ yum -y erase kernel-devel kernel-firmware kernel-headers gcc
 rm -rf /etc/yum.repos.d/{puppetlabs,epel}.repo
 rm -rf VBoxGuestAdditions_*.iso
 
+# Remove Documentation
+rm -rf /usr/share/doc/*
+
+# Remove Locales
+rm -rf /usr/lib/locale/*
+rm -rf /usr/share/locale/*
+
 # Remove traces of mac address from network configuration
 sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
 rm /etc/udev/rules.d/70-persistent-net.rules
