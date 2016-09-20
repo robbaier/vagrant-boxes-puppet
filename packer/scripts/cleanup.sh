437e003a0097b54cd9cea6872431d332aefc954c
Rob <rob@robbaier.com>
2016-09-20 13:17:38 -0500

    Various cleanup

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -2,7 +2,7 @@ yum -y update
 yum -y clean all
 
 yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
-yum -y erase kernel-devel kernel-firmware kernel-headers perl gcc
+yum -y erase kernel-devel kernel-firmware kernel-headers gcc
 
 rm -rf /etc/yum.repos.d/{puppetlabs,epel}.repo
 rm -rf VBoxGuestAdditions_*.iso
